01008f9ddee09c37c31bed68819eaaf4ab07bf4b
goakiller900 <remco.kappers@gmail.com>
2026-07-12 17:37:29 +0200

    Initialize the restored stdlib test harness

--- spec/setup/busted.lua
+++ spec/setup/busted.lua
@@ -1,3 +1,5 @@
--- faketorio needs to be installed on the path somewhere
-require('faketorio')
+-- Load the stdlib-owned Factorio mock harness restored by the CI workflow.
+require('faketorio/searchers')
+require('faketorio/globals')
+
 return require('busted.runner')
